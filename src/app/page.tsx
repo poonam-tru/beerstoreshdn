9c01dff78257aeae01621ae6f9202d57afe3099d
poonam-tru <59247001+poonam-tru@users.noreply.github.com>
2023-10-05 18:13:27 +0530

    read more card module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Slider from "react-slick";
 import InfoCard from "@/modules/infoCard";
 import { Button } from "@/components/ui/button";
+import ReadMoreCard from "@/modules/readMoreCard";
 
 function SampleNextArrow(props: any) {
   const { className, style, onClick } = props;
@@ -223,12 +224,16 @@ const productCardInfo = [
     },
   },
 ];
-const InfoCardDetail = [
+const readMoreDetail = [
   {
+    title: "some day pickup or delivery",
     url: "https://tbsecoms.wpengine.com/wp-content/uploads/2023/08/beerstore.jpg",
+    buttonText: "Order Now",
   },
   {
-    url: "https://tbsecoms.wpengine.com/wp-content/uploads/2023/08/beerstore.jpg",
+    title: "fall into savings",
+    url: "https://tbsecomd.wpengine.com/wp-content/uploads/2023/09/fall-into-savings.jpg",
+    buttonText: "Visit the beerstore hub",
   },
 ];
 const Home = () => {
@@ -239,18 +244,18 @@ const Home = () => {
       <section className="bg-white py-[70px]">
         <div className="container ">
           <div className="grid grid-cols-2 gap-6">
-            {InfoCardDetail.map(({ url }) => (
-              <InfoCard
-                key={`Infocard${url}`}
-                className="flex-row-reverse"
+            {readMoreDetail.map((item) => (
+              <ReadMoreCard
+                className={`bg-[url('https://tbsecomd.wpengine.com/wp-content/uploads/2023/08/cycle_10_tile1_mob.jpg')]`}
+                key={`Infocard${item.url}`}
                 title={"some day pickup or delivery"}
                 image={{
-                  url: url,
-                  alt: "test",
+                  url: item.url,
+                  alt: item.title,
                   width: 200,
                   height: 298,
                 }}
-                buttonText={"Read More"}
+                buttonText={item.buttonText}
               />
             ))}
           </div>
@@ -319,29 +324,27 @@ const Home = () => {
         </div>
         <div className="container">
           <Slider {...BeerRecpieSlidersettings}>
-            
-                {beerStoreCardDetail.map((item, idx) => {
-                  return (
-                    <div className="grid grid-cols-3"  key={`${item.title}`}>
-                    <InfoCard
-                      title={item.title}
-                      image={{
-                        url: item.image.url,
-                        alt: item.image.alt,
-                        width: 200,
-                        height: 298,
-                      }}
-                      className={`${
-                        idx === 0 && "col-span-2"
-                      } rounded-[5px] overflow-hidden`}
-                      buttonText={"Learn More"}
-                      badgeText={"ARTICLE"}
-                      description={"testing"}
-                    />
-                    </div>
-                  );
-                })}
-              
+            {beerStoreCardDetail.map((item, idx) => {
+              return (
+                <div className="grid grid-cols-3" key={`${item.title}`}>
+                  <InfoCard
+                    title={item.title}
+                    image={{
+                      url: item.image.url,
+                      alt: item.image.alt,
+                      width: 200,
+                      height: 298,
+                    }}
+                    className={`${
+                      idx === 0 && "col-span-2"
+                    } rounded-[5px] overflow-hidden`}
+                    buttonText={"Learn More"}
+                    badgeText={"ARTICLE"}
+                    description={"testing"}
+                  />
+                </div>
+              );
+            })}
           </Slider>
         </div>
       </section>
---
+++ src/modules/readMoreCard.tsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
+
+interface readMoreCardProps {
+  title: string;
+  description?: string | JSX.Element;
+  content?: string | JSX.Element;
+  image?: {
+    url: string;
+    alt: string;
+    width: number | `${number}` | undefined;
+    height: number | `${number}` | undefined;
+  };
+  buttonText?: string;
+  badgeText?: string;
+  onClickButton?: () => void;
+  className?: string | undefined;
+  asLink?: boolean;
+  buttonLink?: string | undefined;
+}
+
+const ReadMoreCard = ({
+  title,
+  description,
+  image,
+  buttonText,
+  onClickButton,
+  className,
+  asLink,
+  buttonLink,
+}: readMoreCardProps) => {
+  return (
+    <Card
+      className={cn(
+        " flex flex-row  border border-solid rounded-[10px] relative overflow-hidden",
+        className
+      )}
+    >
+      <CardContent className="py-4 px-2 readmore-content basis-3/5">
+        {title && (
+          <CardTitle className="mb-4 readmore-title uppercase text-white text-[28px]">
+            {title}
+          </CardTitle>
+        )}
+        {description && (
+          <CardDescription className="mb-4 readmore-description">
+            {description}
+          </CardDescription>
+        )}
+        {buttonText && (
+          <Button
+            asChild={asLink}
+            className="w-auto rounded-[100px]"
+            onClick={onClickButton}
+            variant="secondary"
+          >
+            {asLink ? (
+              <Link
+                href={`${buttonLink}`}
+                className={buttonVariants({ variant: "link" })}
+              >
+                {buttonText}
+              </Link>
+            ) : (
+              buttonText
+            )}
+          </Button>
+        )}
+      </CardContent>
+      <div className="flex basis-2/5 ">
+        {image && (
+          <Image
+            src={image.url}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+            style={{
+              width: "100%",
+              height: "auto",
+            }}
+            className="readmore-image"
+          />
+        )}
+      </div>
+    </Card>
+  );
+};
+
+export default ReadMoreCard;
