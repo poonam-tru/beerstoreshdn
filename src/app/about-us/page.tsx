827770559cffd0589aa512622ee67f7bcbd56c7b
poonam-tru <59247001+poonam-tru@users.noreply.github.com>
2023-10-11 17:03:48 +0530

    layout for about and contact

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-    return <h1>about us page</h1>
-  }
+import BannerHeader from "@/modules/bannerHeader";
+import ContainerLayout from "@/modules/containerLayout";
+
+const AboutUs = () => {
+  return (
+    <>
+      <BannerHeader
+        title="About Us"
+        backgroundImage="https://tbsecomd.wpengine.com/wp-content/uploads/2020/02/about-desk.jpg"
+      />
+      <ContainerLayout sidebarContent={<>p</>} content={<>p</>} />
+    </>
+  );
+};
+
+export default AboutUs;
--- src/modules/bannerHeader.tsx
+++ src/modules/bannerHeader.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { relative } from "path";
 
 interface bannerHeader {
   title: string;
-  bannerContent: React.ReactNode;
-  backgroundImage: string;
+  bannerContent?: React.ReactNode;
+  backgroundImage?: string | undefined;
   className?: string | undefined;
 }
 
@@ -16,18 +17,25 @@ const BannerHeader = ({
   className,
 }: bannerHeader) => {
   return (
-    <div className={cn("flex flex-row min-h-[340px]", className)}>
-      <div className="flex flex-row">
-        <Image
-          alt={title}
-          src={backgroundImage}
-          width={100}
-          height={100}
-          style={{ width: "100%", height: "auto" }}
-        />
-      </div>
-      <div className="flex absolute left-0, right-0 justify-center items-center">
-        <h1 className="text-[48px]  text=[#fff]">{title}</h1>
+    <div className={cn("flex flex-row min-h-[340px] relative", className)}>
+      {backgroundImage && (
+        <div className="flex flex-row flex-1">
+          <Image
+            alt={title}
+            src={backgroundImage}
+            width={500}
+            height={300}
+            style={{
+              width: "100%",
+              height: "auto",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+        </div>
+      )}
+      <div className="flex flex-col absolute left-0 h-full right-0 justify-center items-center">
+        {title && <h1 className="text-[48px]  text-white">{title}</h1>}
         {bannerContent}
       </div>
     </div>
